7574a3ab48da56dd332ff226bc9887317c543641
helloitsjoe <jboyle617@gmail.com>
2021-01-17 18:32:34 -0500

    webpack plugin

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,5 +1,32 @@
+// module.exports = env => {
+//   console.log(env);
+//   return {
+//     plugins: ['@snowpack/plugin-babel'],
+//     mount: {
+//       src: '/dist',
+//       public: '/',
+//     },
+//   };
+// };
+
+const { makeWebpackConfig } = require('webpack-simple');
+
+const myConfig = makeWebpackConfig();
+console.log(`myConfig:`, myConfig);
+
 module.exports = {
-  plugins: ['@snowpack/plugin-babel'],
+  plugins: [
+    '@snowpack/plugin-babel',
+    [
+      '@snowpack/plugin-webpack',
+      {
+        extendConfig: config => {
+          // console.log('config', config);
+          return myConfig;
+        },
+      },
+    ],
+  ],
   mount: {
     src: '/dist',
     public: '/',
